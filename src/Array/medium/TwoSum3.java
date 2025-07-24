508e87ddec5d9d289cc83dad1c1b20ddcbb7b09c
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-07-24 17:43:46 +0545

    optimized approach on twosum problem

--- src/Array/medium/TwoSum3.java
+++ src/Array/medium/TwoSum3.java
@@ -3,20 +3,13 @@
 //        int n=nums.length;
 //        HashMap<Integer,Integer> map=new HashMap<>();
 //        for(int i=0;i<n;i++){
-//            map.put(nums[i],i);
-
-//        }
-//
-//
-//        for(int i=0;i<nums.length;i++){
-//            int k=target-nums[i];
-
-
-//            if(map.containsKey(k)&& map.get(k)!=i){
-//                return new int[]{i,map.get(k)};
+//            int complement=target-nums[i];
+//            if(map.containsKey(complement)){
+//                return new int[]{map.get(complement),i};
 //            }
+//            map.put(nums[i],i);
+//
 //        }
 //        return new int[]{-1,-1};
-
 //    }
 //}
