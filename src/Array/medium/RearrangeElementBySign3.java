ad9488665f6e39526639802d8a4000205bcf6b54
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-08-09 18:08:55 +0545

    rearrange element by sign brute force done using arraylist data structure using automatic arraylist to arrayconvert using stream function

---
+++ src/Array/medium/RearrangeElementBySign3.java
@@ -0,0 +1,27 @@
+//class Solution {
+//    public int[] rearrangeArray(int[] nums) {
+//        int n=nums.length;
+//        ArrayList<Integer> v1=new ArrayList<>();
+//        ArrayList<Integer> v2=new ArrayList<>();
+//        ArrayList<Integer> ans=new ArrayList<>();
+//
+//        for(int num:nums){
+//            if(num>0){
+//                v1.add(num);
+//            }else{
+//                v2.add(num);
+//            }
+//        }
+//
+//        int ind1=0;
+//        int ind2=0;
+//        while(ind1<(n/2)){
+//            ans.add(v1.get(ind1));
+//            ind1++;
+//            ans.add(v2.get(ind2));
+//            ind2++;
+//        }
+//
+//        return ans.stream().mapToInt(Integer::intValue).toArray();
+//    }
+//}
