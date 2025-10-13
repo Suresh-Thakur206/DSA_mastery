a9a8a86e38524a47bc3c5a91c4f3f555ee74a240
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-10-13 21:39:47 +0545

    remove parenthesis optimal solution done

---
+++ src/BinarySearch/medium/RotatedArray2.java
@@ -0,0 +1,16 @@
+//package BinarySearch.medium;
+//import java.util.Scanner;
+//
+//public class RotatedArray2 {
+//    public static int RotatedArray(int[]nums,int target){
+//
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the target :");
+//        int target=sc.nextInt();
+//        int []nums={2,5,6,0,0,1,2};
+//        int n=RotatedArray(nums,target);
+//
+//    }
+//}
--- src/test/SetBuilderLearn.java
+++ src/test/SetBuilderLearn.java
@@ -2,13 +2,15 @@ package test;
 import java.util.Scanner;
 public class SetBuilderLearn {
     public static void main(String[] args) {
-        StringBuilder sb=new StringBuilder();
-        sb.append("thsi");
-        sb.append("is");
-        sb.append("the");
-        sb.append("world");
-        String result=sb.toString();
-        System.out.println(result);
+//        StringBuilder sb=new StringBuilder();
+//        sb.append("thsi");
+//        sb.append("is");
+//        sb.append("the");
+//        sb.append("world");
+//        String result=sb.toString();
+//        System.out.println(result);
+
+
 
     }
 }
