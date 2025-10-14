34c2714b719505d7588159b0fc4e1d75e03d8f02
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-10-14 11:12:15 +0545

    optimal solution for minimum element in rotated array using binary search done

--- src/BinarySearch/medium/RotatedArray2.java
+++ src/BinarySearch/medium/RotatedArray2.java
@@ -1,16 +1,38 @@
-//package BinarySearch.medium;
-//import java.util.Scanner;
-//
-//public class RotatedArray2 {
-//    public static int RotatedArray(int[]nums,int target){
-//
-//    }
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("Enter the target :");
-//        int target=sc.nextInt();
-//        int []nums={2,5,6,0,0,1,2};
-//        int n=RotatedArray(nums,target);
-//
-//    }
-//}
+package BinarySearch.medium;
+import java.util.Scanner;
+
+public class RotatedArray2 {
+    public static int RotatedArray(int[]nums,int target){
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if (nums[left]<=nums[mid]){
+                if(nums[left]<=target&& target<=nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the target :");
+        int target=sc.nextInt();
+        int []nums={4,5,6,7,0,1,2};
+        int n=RotatedArray(nums,target);
+        System.out.println("index is : "+n);
+
+    }
+}
