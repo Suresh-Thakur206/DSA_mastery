734002a0628edee22ccbc787ed45508dc20fcd3a
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-10-16 08:15:50 +0545

    brute force for find kth rotation

---
+++ src/BinarySearch/Easy/FindKthRotationBrute.java
@@ -0,0 +1,12 @@
+//class Solution {
+//    public int findKRotation(int arr[]) {
+//        // Code here
+//        int k=0;
+//        for(int i=0;i<arr.length-1;i++){
+//            if(arr[i]>arr[i+1]){
+//                return (k+i+1);
+//            }
+//        }
+//        return k;
+//    }
+//}
