8b6671ab26eaec8558a506f9ac4c7695bfb22d57
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-03 20:57:27 +0545

    brute force solution for floor in sorted array

---
+++ src/BinarySearch/medium/FloorBrute.java
@@ -0,0 +1,14 @@
+//
+//
+//class Solution {
+//    public int findFloor(int[] arr, int x) {
+//        // code here
+//        int floor=-1;
+//        for(int i=0; i<arr.length;i++){
+//            if(arr[i]<=x){
+//                floor=i;
+//            }
+//        }
+//        return floor;
+//    }
+//}
