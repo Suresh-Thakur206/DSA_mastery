c5e71a04135c634dfda9afc03e9f17dd00373611
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-04 13:07:36 +0545

    traversal through linkedlist,total count of linkedlist and searching element in linkedlist are done

--- src/tutorial/Linkedlist2.java
+++ src/tutorial/Linkedlist2.java
@@ -25,9 +25,33 @@ public class Linkedlist2 {
         }
         return head;
     }
-    public static void main(String[] args) {
-        int []arr={12,5,6,8};
-        Node1 head=converArr2LL(arr);
-        System.out.println(head.data);
+
+    //lengthh of linkedlist
+    public static int lengthLL(Node1 head){
+        int cnt=0;
+        Node1 temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            cnt++;
+        }
+        return cnt;
     }
+    // serching
+    public static int checkLL(Node1 head,int value){
+        Node1 temp=head;
+        while(temp!=null){
+            if(temp.data==value){
+                return 1;
+            }
+            temp=temp.next;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int []arr={12,5,6,8,7,9};
+        Node1 head=converArr2LL(arr);
+       // System.out.println(lengthLL(head));
+        System.out.println(checkLL(head,3));
+
+}
 }
