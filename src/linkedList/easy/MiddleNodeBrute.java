a7dfa1733a06037a6590f38af83740d18f37bf78
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-15 16:32:27 +0545

    feat:Add Middle  Node of LinkedList Brute Force Solution

---
+++ src/linkedList/easy/MiddleNodeBrute.java
@@ -0,0 +1,28 @@
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+//class Solution {
+//    public ListNode middleNode(ListNode head) {
+//
+//        ListNode temp=head;
+//        int cnt=0;
+//        while(temp!=null){
+//            cnt++;
+//            temp=temp.next;
+//        }
+//
+//        int mid=cnt/2;
+//        ListNode temp1=head;
+//        for(int i=0;i<mid;i++){
+//            temp1=temp1.next;
+//        }
+//        return temp1;
+//    }
+//}
