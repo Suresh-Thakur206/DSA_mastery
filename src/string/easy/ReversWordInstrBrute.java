05192ea9ad08418ba1b7baa046ead79001eac4aa
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-22 09:33:07 +0545

    feat:Add Reverse Words in strings

--- src/string/easy/ReversWordInstrBrute.java
+++ src/string/easy/ReversWordInstrBrute.java
@@ -1,4 +1,20 @@
-package string.easy;
-
-public class ReversWordInstrBrute {
+//151. Reverse Words in a String
+// Brute force Solution
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder result=new StringBuilder();
+        String s1[]=s.trim().split("\\s+");
+        int len=s1.length;
+        for(int i=len-1;i>=0;i--){
+            if(s1[i]==""){
+                continue;
+            }
+            if(result.length()==0){
+                result.append(s1[i]);
+            }else{
+                result.append(" ").append(s1[i]);
+            }
+        }
+        return result.toString();
+    }
 }
