3d70cd33c1824310ad9bc0a40d368ae56e8f6c94
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-22 12:46:33 +0545

    feat:Add Optimal Solution for Largest Odd Number in String

---
+++ src/string/easy/LargestOddNumOpt.java
@@ -0,0 +1,14 @@
+// 1903. Largest Odd Number in String
+//Optimal Solution
+
+class Solution {
+    public String largestOddNumber(String num) {
+        int len=num.length();
+        for(int i=len-1;i>=0;i--){
+            if((num.charAt(i)-'0')%2!=0){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
