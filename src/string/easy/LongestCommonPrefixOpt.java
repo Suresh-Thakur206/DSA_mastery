90dc788f86f9039b9a76d9e81a3198fc810399e8
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-23 13:09:11 +0545

    feat:Add Optimal Solution for Longest Common Prefix in String

---
+++ src/string/easy/LongestCommonPrefixOpt.java
@@ -0,0 +1,23 @@
+
+
+// 14. Longest Common Prefix
+// Optimal Solution
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0){
+            return "";
+        }
+
+        String first=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(first)){
+                first=first.substring(0, first.length()-1);
+
+                if(first.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return first;
+    }
+}
