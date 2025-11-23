46adf148cd5cda91c64c7db946eb6098820398aa
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-11-23 17:45:33 +0545

    feat:Add Optimal Solution for Rotate String

---
+++ src/string/medium/RotateStringOpt.java
@@ -0,0 +1,15 @@
+
+// 796. Rotate String
+// optimal Solution
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if( s.length() != goal.length()){
+            return false;
+        }
+        String rotate=s+s;
+        if(rotate.contains(goal)){
+            return true;
+        }
+        return false;
+    }
+}
