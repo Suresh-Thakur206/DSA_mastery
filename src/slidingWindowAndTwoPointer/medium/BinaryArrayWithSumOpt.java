18a00f406aba879cfb899c9acc2f8db1f77ef34e
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-12-03 09:30:06 +0545

    feat:Add Optimal Solution for Binary SubArrays With Sum

--- src/slidingWindowAndTwoPointer/medium/BinaryArrayWithSumOpt.java
+++ src/slidingWindowAndTwoPointer/medium/BinaryArrayWithSumOpt.java
@@ -1,4 +1,25 @@
-package slidingWindowAndTwoPointer.medium;
+// 930. Binary Subarrays With Sum
+// Optimal Solution for Binary SubArrays With Sum
 
-public class BinaryArrayWithSumOpt {
+class Solution {
+    public int numSubarraysWithSumAtMostK(int[] nums, int goal){
+        int start=0;
+        int sum=0;
+        int count=0;
+        for(int end=0;end<nums.length;end++){
+            sum+=nums[end];
+            while(start<=end &&sum>goal){
+                sum-=nums[start];
+                start++;
+            }
+            count+=(end-start+1);
+        }
+        return count;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        if(goal<0){
+            return 0;
+        }
+        return numSubarraysWithSumAtMostK( nums, goal)- numSubarraysWithSumAtMostK( nums,  goal-1);
+    }
 }
