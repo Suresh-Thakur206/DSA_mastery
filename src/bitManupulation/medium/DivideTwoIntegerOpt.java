db97137ffbb2ba81a35bae1cd0edec829e0beb34
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-12-08 18:53:07 +0545

    feat:Add Optimal Solution for Divide Two Integers

---
+++ src/bitManupulation/medium/DivideTwoIntegerOpt.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend==divisor){
+            return 1;
+        }
+
+        boolean sign=true;
+        if(dividend>=0 && divisor<0){
+            sign=false;
+        }else if(dividend<0 && divisor>=0){
+            sign=false;
+        }
+        long n=Math.abs((long)dividend);
+        long d=Math.abs((long)divisor);
+
+        long ans=0;
+        while(n>=d){
+            int cnt=0;
+            while(n>=(d<<(cnt+1))){
+                cnt++;
+            }
+            ans+=1<<cnt;
+            n-=d<<cnt;
+        }
+        if(ans==1<<31 && sign){
+            return Integer.MAX_VALUE;
+        }
+        if(ans==1<<31 && !sign){
+            return Integer.MIN_VALUE;
+        }
+        return (sign==false)?(int)-ans:(int)ans;
+    }
+}
