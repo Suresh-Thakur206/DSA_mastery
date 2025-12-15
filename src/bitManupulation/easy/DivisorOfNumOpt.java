e2b96f285b008944fb111084b66f08af3f9ba57e
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-12-15 11:41:38 +0545

    feat:Add Optimal Solution for All Divisor of Number

---
+++ src/bitManupulation/easy/DivisorOfNumOpt.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public static void print_divisors(int n) {
+        ArrayList<Integer> small = new ArrayList<>();
+        ArrayList<Integer> large = new ArrayList<>();
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                small.add(i);
+                if (i != n / i) {
+                    large.add(n / i);
+                }
+            }
+        }
+
+        // Print in increasing order
+        for (int x : small) {
+            System.out.print(x + " ");
+        }
+        for (int i = large.size() - 1; i >= 0; i--) {
+            System.out.print(large.get(i) + " ");
+        }
+    }
+}
