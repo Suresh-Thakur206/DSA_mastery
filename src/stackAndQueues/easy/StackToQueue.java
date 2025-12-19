245f37218fcaab5668067f07fe4f98a26f3edbd2
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-12-19 08:53:13 +0545

    feat:Add Implementation of Queue using Stack

---
+++ src/stackAndQueues/easy/StackToQueue.java
@@ -0,0 +1,44 @@
+class MyQueue {
+    Stack<Integer> in;
+    Stack<Integer> out;
+    public MyQueue() {
+        in=new Stack<>();
+        out=new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+    }
+
+    public int pop() {
+        moveIfNeeded();
+        int k=out.pop();
+        return k;
+
+    }
+
+    public int peek() {
+        moveIfNeeded();
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return (in.isEmpty()&& out.isEmpty());
+    }
+    private void moveIfNeeded(){
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
