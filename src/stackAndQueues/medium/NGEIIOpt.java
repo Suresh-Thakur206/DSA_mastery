b63cd12371e14c61b45ce7620d31d4298ffbdc9e
Suresh_Thakur <sureshthakur2080@gmail.com>
2026-01-08 13:55:30 +0545

    feat:add Optimal Solution for Next Greater Number in Circular Array

---
+++ src/stackAndQueues/medium/NGEIIOpt.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stack=new Stack<>();
+
+        int n=nums.length;
+        int []result=new int[n];
+        for(int i=2*n-1;i>=0;i--){
+            while(!stack.isEmpty()&& stack.peek()<=nums[i%n]){
+                stack.pop();
+            }
+            if(i<n){
+                result[i]=stack.isEmpty()? -1:stack.peek();
+
+            }
+            stack.push(nums[i%n]);
+        }
+        return result;
+    }
+}
