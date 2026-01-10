9b1cbb97dd01dbd3591f825871e7b732070be431
Suresh_Thakur <sureshthakur2080@gmail.com>
2026-01-10 13:53:00 +0545

    feat:Add Optimal Solution for Sum of Subarray Minimum

---
+++ src/stackAndQueues/medium/SumOfArrayMinOpt.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] findNSE(int[] arr){
+        int n=arr.length;
+        int []ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=n;
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int[] findPSEE(int []arr){
+        int n=arr.length;
+        int []ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=-1;
+            }else{
+                ans[i]=st.peek();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    public int sumSubarrayMins(int[] arr) {
+        int n=arr.length;
+        int []nse=findNSE(arr);
+        int []psee=findPSEE(arr);
+
+        int mod=(int)1e9+7;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int left=i-psee[i];
+            int right=nse[i]-i;
+            long freq=left*right*1L;
+            int val=(int)((freq*arr[i])%mod);
+            sum=(sum+val)%mod;
+        }
+        return sum;
+
+    }
+}
