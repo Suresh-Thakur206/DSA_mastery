cfed620b22713dc1833a8333f52685c00dc0f157
Suresh_Thakur <sureshthakur2080@gmail.com>
2026-01-31 08:20:25 +0545

    revert "src/heaps/easy/LastStoneWeightBrute1.java"
    This reverts complete solution.

---
+++ src/Array/easy/KthLargestElementInstremBrute.java
@@ -0,0 +1,35 @@
+class KthLargest {
+    int k;
+    ArrayList<Integer> list=new ArrayList<>();
+
+
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        for(int i=0;i<nums.length;i++){
+            list.add(nums[i]);
+        }
+
+    }
+
+    public int add(int val) {
+        list.add(val);
+        for(int i=0;i<list.size()-1;i++){
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(j)>list.get(i)){
+                    int temp=list.get(i);
+                    list.set(i,list.get(j));
+                    list.set(j,temp);
+                }
+            }
+        }
+        int value=list.get(k-1);
+        return value;
+
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
