4ba7ec7e49e2193eb0cf669226b50b2cd633d7ed
Suresh_Thakur <sureshthakur2080@gmail.com>
2026-02-01 22:07:27 +0545

    Add Optimal Solution for K Closest Points to Origin

---
+++ src/heaps/medium/KclosesPointOpt.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]>queue=new PriorityQueue<>(
+                (a,b)->(a[0]*a[0] + a[1]*a[1])-(b[0]*b[0]+b[1]*b[1])
+        );
+        for(int[] point:points){
+            queue.offer(point);
+        }
+        int[][]res=new int[k][2];
+        for(int i=0;i<k;i++){
+            res[i]=queue.poll();
+        }
+        return res;
+    }
+}
