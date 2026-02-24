f29030d4b148bfeb7170ae0089943f202ef3da71
Suresh_Thakur <sureshthakur2080@gmail.com>
2026-02-24 22:26:43 +0545

    Add Optimal Solution for Remove Element

---
+++ src/Array/easy/RemoveElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            if(nums[left]==val){
+                nums[left]=nums[right];
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return left;
+    }
+}
