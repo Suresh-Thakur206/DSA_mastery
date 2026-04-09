4592bfe1bf072e30cc41886047f2bc1b1968fc2b
Suresh-Thakur206 <sureshthakur2080@gmail.com>
2026-04-09 17:40:18 -0400

    Add Optimal Solution for Valied Sudoku

---
+++ src/Array/medium/ValiedSudokuOpt.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> st=new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char c=board[i][j];
+                String row=c+"row"+i;
+                String col=c+"col"+j;
+                String box=c+"box"+(i/3)+"_"+(j/3);
+                if(c=='.') continue;
+                if(st.contains(row) || st.contains(col) || st.contains(box)) return false;
+                st.add(row);
+                st.add(col);
+                st.add(box);
+            }
+        }
+        return true;
+    }
+}
